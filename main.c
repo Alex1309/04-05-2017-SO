8fcf3cd1eb65cc04c564659f38c237da2ad479ea
Alex1309 <lopez.alexandra@correounivalle.edu.co>
2017-04-05 16:02:46 -0500

    primer commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "fibo.h"
+#include <stdio.h>
+
+int main(int arg, char ** argc){
+	
+	printf("%d\n",fibonacci(5));
+}
+
+
---
+++ oper.h
@@ -0,0 +1,6 @@
+void suma(int *,int);
+void resta(int *,int);
+void sumav(int *,int *,int );
+void restav(int *,int *,int );
+void sumav(int *,int *,int );
+void multescalar(int *,int *,int *,int);
